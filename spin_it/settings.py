d94eb9173a9de907cd0b8bef29985b0e92cc0e88
Carlos Navotka <knavotka@hotmail.com>
2022-01-14 13:42:09 +0000

    Install Stripe, give functionality to Payments Intents and added Public and Secret keys

--- spin_it/settings.py
+++ spin_it/settings.py
@@ -204,5 +204,10 @@ if 'USE_AWS' in os.environ:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 30
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
